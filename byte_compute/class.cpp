ed5c7e22f56aaed12763511346c7aad639278e89
Yuxin <956163372@qq.com>
2019-10-31 14:50:02 +0800

    更新

--- byte_compute/class.cpp
+++ byte_compute/class.cpp
@@ -3,23 +3,29 @@
 using namespace std;
 class Person {
   public:
-    Person(string name) {
+    Person(string name, int age) {
       personName = name;
+      personAge = age;
       cout << personName << endl;
     }
     string getName() {
       return personName;
     }
     int getAge() {
+      string name = this->getName();
+
       return personAge;
     }
   private:
     string personName;
-    int personAge = 12;
+    int personAge;
 };
 
 int main () {
-  Person person("张三");
-  cout << person.getAge() << endl;
+  Person person("张三", 18);
+  Person* pointer = &person;
+  Person* pointer_one = new Person("李四", 22);
+  cout << pointer->getAge() << endl;
+  cout << pointer_one->getName() << endl;
   return 0;
 }
