511b46b75dffc391f01d2cbffb6b6b61ba31074d
Yuxin <9561633712@qq.com>
2019-11-18 01:16:26 +0800

    更新

--- base_class/sin_direction_list.ts
+++ base_class/sin_direction_list.ts
@@ -1,13 +1,44 @@
 export class Node {
-    data: number
-    next: Node | null
-    constructor(data: number) {
-        this.data = data
-    }
+  data: number
+  next: Node | null
+  constructor(data: number) {
+    this.data = data
+  }
 }
 export class SinDirectionList {
-    head: Node | null
-    // TODO 对单项列表的继续封装
+  head: Node | null
+  length: number
+  constructor() {
+    this.length = 0
+  }
+  findNode(data: number): number {
+    let position: number = 0
+    let currentNode: Node = this.head
+    if(!currentNode) {
+      throw new Error('Attention: The List does not have any Node')
+    }
+    return
+  }
+  insertNode(node: Node, pos: number): void {
+    if(!this.head && !pos) {
+      this.head = node
+      return
+    }
+    let index: number = 0
+    let currentNode: Node = this.head
+    while(currentNode.next) {
+      currentNode = currentNode.next
+      index ++
+    }
+    if(index < pos) {
+      throw new Error('Error Input Position')
+    } else if(index === pos) {
+      currentNode.next = node
+    } else {
+      node.next = currentNode.next
+      currentNode.next = node
+    }
+  }
 }
 let node = new Node(8)
 console.log(node);
